29b0280c76f6ed898b0388197bc0c5dc9c6b1f1c
magizh <magizh@okta.com>
2021-07-22 15:45:42 -0700

    feat: OIE custom app push challenge
    * Adds push challenge logic for Custom app authenticator.
    * This PR does not cover support for enroll or Number matching challenge yet.
    * Icon logo support is in a different pr.
    
    RESOLVES: OKTA-413800

--- playground/mocks/config/responseConfig.js
+++ playground/mocks/config/responseConfig.js
@@ -97,7 +97,7 @@ const idx = {
     'error-authenticator-enroll-phone-invalid-number',
   ],
   '/idp/idx/identify': [
-    'authenticator-enroll-select-authenticator',
+    //'authenticator-enroll-select-authenticator',
     // 'identify-with-only-one-third-party-idp',
     // 'error-identify-access-denied',
     // 'error-identify-user-locked-unable-challenge',
@@ -139,6 +139,8 @@ const idx = {
     // 'enroll-profile-new'
     // 'authenticator-enroll-email',
     // 'authenticator-verification-okta-verify-push',
+    // 'authenticator-verification-custom-app-push',
+    // authenticator-verification-custom-app-push-reject',
   ],
   '/idp/idx/challenge': [
     // 'authenticator-verification-webauthn',
